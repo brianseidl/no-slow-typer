c2b577af36aec216437a0c102ec852a540657a7e
Brian Seidl <brian.seidl@coxautoinc.com>
2020-10-01 22:33:13 -0400

    some colors and what not

--- src/components/Char.js
+++ src/components/Char.js
@@ -1,24 +1,45 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 
 class Char extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isFocus: false,
-            value: null,
-            isInputed: false,
-            isCorrect: null,
-            isNext: false,
+            value: this.props.value,
+            status: this.props.status
         };
     }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.status !== this.state.status) {
+            this.setState({status: nextProps.status});
+        }
+    }
+
     render() {
+        let color;
+        if (this.state.status === "inFocus") {
+            color = "blue";
+        } else if (this.state.status === "correct") {
+            color = "green";
+        } else if (this.state.status === "incorrect") {
+            color = "red";
+        } else if (this.state.status === "notEntered") {
+            color = "black";
+        }
+
         return (
-            <div className="typer-char">
-                {this.props.value}
+            <div className="typer-char" style={{"color": color}}>
+                {this.state.value}
             </div>
         );
     }
+};
+
+Char.propTypes = {
+    value: PropTypes.string.isRequired,
+    status: PropTypes.oneOf(['inFocus', 'correct', 'incorrect', 'notEntered']).isRequired
 };
 
 export default Char;
--- src/components/Typer.js
+++ src/components/Typer.js
@@ -4,47 +4,77 @@ import Char from './Char.js';
 
 class Typer extends Component {
 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            index: 0,
+            text: "Build apps, libraries, frameworks, plug-ins, and other executable code that run natively on Apple silicon. When you build executables on top of Apple frameworks and technologies, the only significant step you might need to take is to recompile your code for the arm64 architecture. If you rely on hardware-specific details or make assumptions about low-level features, modify your code as needed to support Apple silicon.",
+            wrong: []
+        };
+    }
+
+    handleKeyPress = (event) => {
+        // Read the keyboard input char
+        let key = event.key;
+        if (key === this.state.text[this.state.index]){
+            this.setState({index: this.state.index + 1});
+        } else {
+            // is wrong
+            this.setState(prevState => ({
+                wrong: [prevState.wrong, this.state.index],
+                index: this.state.index + 1
+            }));
+        }
+    }
+
+    handleKeyDown = (event) => {
+        // Basically look for backspace
+        let keyCode = event.keyCode;
+        if (keyCode === 8 && this.state.index > 0) {
+            this.setState({
+                wrong: this.state.wrong.filter((_, i) => _ !== this.state.index - 1),
+                index: this.state.index - 1
+            });
+        }
+    }
+
+    getFocus = () => {
+        var inputBox = document.getElementById("inputBox");
+        inputBox.focus();
+    }
+
     render() {
-        var text = "Build apps, libraries, frameworks, plug-ins, and other executable code that run natively on Apple silicon. When you build executables on top of Apple frameworks and technologies, the only significant step you might need to take is to recompile your code for the arm64 architecture. If you rely on hardware-specific details or make assumptions about low-level features, modify your code as needed to support Apple silicon.";
-        var chars = text.split('');
+        var chars = this.state.text.split('');
         const charblock = chars.map((c, i) => {
+            let status = "notEntered";
+            
+            if (i === this.state.index) {
+                status = "inFocus"
+            } else if (i < this.state.index) {
+                status = "correct"
+            }
+            
+            if (this.state.wrong.includes(i)) {
+                status = "incorrect"
+            }
+            
             return (
-                <Char key={i} value={c} />
+                <Char status={status} value={c} />
             );
         });
-        var index = 0;
-
-        function keydown(e) {
-            var keycode = e.keyCode;
-            // alert(keycode);
-            if ((((keycode >= 65) && (keycode <= 90))
-            || ((keycode >= 48) && (keycode <= 57))
-            || ((keycode >= 186) && (keycode <= 222))) && (index >= -1)) {
-                var i = 0;
-                i = index;
-                index++;
-                alert(index);
-                return (
-                    <Char key={i} isFocus={true} isInputed={false} />
-                );
-            }
-            else if ((keycode === 8) && (index >= -1)) {
-                index--;
-                alert(index);
-            }
-        }
-
-        function getFocus() {
-            var inputBox = document.getElementById("inputBox");
-            inputBox.focus();
-        }
 
         return (
-            <div className="typer" onClick={getFocus}>
-                <div className="typer-board">
-                    {charblock}
+            <div>
+                <div className="typer" onClick={this.getFocus}>
+                    <div className="typer-board">
+                        {charblock}
+                    </div>
+                    <input id="inputBox" autoFocus="autoFocus" className="input-hidden" onKeyPress={this.handleKeyPress} onKeyDown={this.handleKeyDown}></input>
                 </div>
-                <input id="inputBox" autoFocus="autoFocus" className="input-hidden" onKeyDown={keydown}></input>
+                <div>
+                    <p>index: {this.state.index}</p>
+                </div>
             </div>
         );
     }
