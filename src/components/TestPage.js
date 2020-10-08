26c3e582c516d92fd1aba573d91364f527c06a07
Yilun Yang <65271505+YilunYang-Adelphi@users.noreply.github.com>
2020-10-08 19:52:21 -0400

    Update TestPage.js

--- src/components/TestPage.js
+++ src/components/TestPage.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Typer from './Typer.js';
 
 
-export class Test extends Component {
+class Test extends Component {
   render() {
     return (<Typer/>);
   }
