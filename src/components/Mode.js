ef2958f914fd70daeb8831e6a734846424a7e4f8
yilun yang <yilunyang@mail.adelphi.edu>
2020-12-17 16:41:58 -0500

    some changes

--- src/components/Mode.js
+++ src/components/Mode.js
@@ -45,14 +45,32 @@ class Mode extends React.Component {
         <Description description={this.state.data} />
         <div className="Mode-Group">
 
-          <div id="short_btn" className="Mode-Selection Mode-Selection-Short" onMouseEnter={() => this.setNewNumber("s")}>
-            <a href="/test/short" className="Mode-Button">Short</a>
+          <div 
+            id="short_btn" 
+            className="Mode-Selection Mode-Selection-Short" 
+            onMouseEnter={() => this.setNewNumber("s")}
+          >
+            <a href="/test/short" className="Mode-Button">
+              Short
+            </a>
           </div>
-          <div id="medium_btn" className="Mode-Selection Mode-Selection-Medium" onMouseEnter={() => this.setNewNumber("m")}>
-            <a href="/test/medium" className="Mode-Button">Medium</a>
+          <div 
+            id="medium_btn" 
+            className="Mode-Selection Mode-Selection-Medium" 
+            onMouseEnter={() => this.setNewNumber("m")}
+          >
+            <a href="/test/medium" className="Mode-Button">
+              Medium
+            </a>
           </div>
-          <div id="long_btn" className="Mode-Selection Mode-Selection-Long" onMouseEnter={() => this.setNewNumber("l")}>
-            <a href="/test/long" className="Mode-Button">Long</a>
+          <div 
+            id="long_btn" 
+            className="Mode-Selection Mode-Selection-Long" 
+            onMouseEnter={() => this.setNewNumber("l")}
+          >
+            <a href="/test/long" className="Mode-Button">
+              Long
+            </a>
           </div>
         </div>
       </div>
